fecf57644d3ff2dcecdd25d17ba5e91474b55d18
Angel J. Haro <haro.j.angel@gmail.com>
2024-03-27 16:11:50 -0700

    line block per language

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -12,31 +12,27 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        -- LUA
+        -- LUA -----------------------------------------
         null_ls.builtins.formatting.stylua,
-        -- JAVA
+        -- JAVA -----------------------------------------
         null_ls.builtins.formatting.google_java_format,
         null_ls.builtins.diagnostics.checkstyle.with({
           extra_args = { "-c", "/google_checks.xml" },
         }),
-        -- BASH
+        -- BASH -----------------------------------------
         null_ls.builtins.formatting.shfmt,
         require("none-ls-shellcheck.diagnostics"),
         require("none-ls-shellcheck.code_actions"),
-
-        ------------------------------------------------
-        -- JS | TS | HTML | CSS and more...
+        -- JS | TS | HTML | CSS -------------------------
         null_ls.builtins.formatting.prettier.with({
           filetypes = { "html", "json", "yaml", "markdown" },
           extra_filetypes = { "toml" },
         }),
         require("none-ls.diagnostics.eslint_d"),
-        ------------------------------------------------
-
-        -- GO
+        -- GO -----------------------------------------
         null_ls.builtins.diagnostics.staticcheck,
         null_ls.builtins.formatting.asmfmt,
-        -- PYTHON
+        -- PYTHON -------------------------------------
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
       },
