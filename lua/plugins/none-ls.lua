01824f2505be0854dd8e5e949407c45c1a8ba7cb
Angel J. Haro <haro.j.angel@gmail.com>
2024-03-28 22:30:37 -0700

    fixing JAVA CONFIG

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,46 +1,46 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-    dependencies = {
-      "gbprod/none-ls-shellcheck.nvim", -- shellcheck
-      "nvimtools/none-ls-extras.nvim", -- eslint_d
-    },
-  },
-  config = function()
-    -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- LUA -----------------------------------------
-        null_ls.builtins.formatting.stylua,
-        -- JAVA -----------------------------------------
-        null_ls.builtins.formatting.google_java_format,
-        null_ls.builtins.diagnostics.checkstyle.with({
-          extra_args = { "-c", "/google_checks.xml" },
-        }),
-        -- BASH -----------------------------------------
-        null_ls.builtins.formatting.shfmt,
-        require("none-ls-shellcheck.diagnostics"),
-        require("none-ls-shellcheck.code_actions"),
-        -- JS | TS | HTML | CSS -------------------------
-        null_ls.builtins.formatting.prettier.with({
-          filetypes = { "html", "json", "yaml", "markdown" },
-          extra_filetypes = { "toml" },
-        }),
-        require("none-ls.diagnostics.eslint_d"),
-        -- GO -----------------------------------------
-        null_ls.builtins.diagnostics.staticcheck,
-        null_ls.builtins.formatting.asmfmt,
-        -- PYTHON -------------------------------------
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"gbprod/none-ls-shellcheck.nvim", -- shellcheck
+		"nvimtools/none-ls-extras.nvim", -- eslint_d
+	},
+	config = function()
+		-- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- LUA -----------------------------------------
+				null_ls.builtins.formatting.stylua,
+				-- JAVA -----------------------------------------
+				-- null_ls.builtins.formatting.google_java_format, -- ***WE USE FTPLUGIN/JAVA.LUA***
+				null_ls.builtins.diagnostics.checkstyle.with({
+					extra_args = { "-c", "/google_checks.xml" },
+				}),
+				-- BASH -----------------------------------------
+				null_ls.builtins.formatting.shfmt,
+				require("none-ls-shellcheck.diagnostics"),
+				require("none-ls-shellcheck.code_actions"),
+				-- JS | TS | HTML | CSS -------------------------
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = { "html", "json", "yaml", "markdown" },
+					extra_filetypes = { "toml" },
+				}),
+				require("none-ls.diagnostics.eslint_d"),
+				-- GO -----------------------------------------
+				null_ls.builtins.diagnostics.staticcheck,
+				null_ls.builtins.formatting.asmfmt,
+				-- PYTHON -------------------------------------
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+			},
+		})
 
-    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+	end,
 }
+
+-- diagnostic == linting
+-- formatting == format
 
 -- diagnostic == linting
 -- formatting == format
