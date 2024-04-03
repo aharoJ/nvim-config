bf24bf09387b3c250154a92e06f539a25499ccdc
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-02 22:33:39 -0700

    Reference LSP Updates... injected formatting | debugging

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,47 +1,69 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"gbprod/none-ls-shellcheck.nvim", -- shellcheck
-		"nvimtools/none-ls-extras.nvim", -- eslint_d
-	},
-	config = function()
-		-- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- LUA -----------------------------------------
-				null_ls.builtins.formatting.stylua,
-				-- JAVA -----------------------------------------
-				-- null_ls.builtins.formatting.google_java_format, -- ***WE USE FTPLUGIN/JAVA.LUA***
-				-- null_ls.builtins.diagnostics.checkstyle, -- ***WE USE FTPLUGIN/JAVA.LUA***
-				-- BASH -----------------------------------------
-				null_ls.builtins.formatting.shfmt,
-				require("none-ls-shellcheck.diagnostics"),
-				require("none-ls-shellcheck.code_actions"),
-				-- JS | TS | HTML | CSS -------------------------
-				null_ls.builtins.formatting.prettier.with({
-					filetypes = { "html", "json", "yaml", "markdown" },
-					extra_filetypes = { "toml" },
-				}),
-				require("none-ls.diagnostics.eslint_d"),
-				-- GO -----------------------------------------
-				null_ls.builtins.diagnostics.staticcheck,
-				null_ls.builtins.formatting.asmfmt,
-				-- PYTHON -------------------------------------
-				-- null_ls.builtins.formatting.black,
-				-- null_ls.builtins.formatting.isort,
-				-- XML -------------------------------------
-				null_ls.builtins.formatting.tidy,
-				null_ls.builtins.diagnostics.tidy,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "gbprod/none-ls-shellcheck.nvim", -- shellcheck
+    "nvimtools/none-ls-extras.nvim", -- eslint_d
+  },
+  config = function()
+    -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        -------------------        JAVA       ------------------------
+        -- formatting -- ***WE USE FTPLUGIN/JAVA.LUA***
+        --  diagnostics -- ***WE USE FTPLUGIN/JAVA.LUA***
+        ----------------                              ----------------
 
-		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-	end,
+        -------------------        LUA       ------------------------
+        null_ls.builtins.formatting.stylua,
+        --  diagnostics -- ***WE USE lua_ls**
+        ----------------                              ----------------
+
+        -------------------        BASH       ------------------------
+        null_ls.builtins.formatting.shfmt,
+        require("none-ls-shellcheck.diagnostics"),
+        require("none-ls-shellcheck.code_actions"),
+        ----------------                              ----------------
+
+        -------------------        TS | JS       ------------------------
+        null_ls.builtins.formatting.prettierd,
+        require("none-ls.diagnostics.eslint_d"), -- TS | JS
+        -------------------        HTML       ------------------------
+        null_ls.builtins.diagnostics.markdownlint, -- HTML
+        -------------------        CSS       ------------------------
+        null_ls.builtins.diagnostics.stylelint, -- CSS
+        ----------------                              ----------------
+
+        -------------------        GO       ------------------------
+        null_ls.builtins.diagnostics.staticcheck,
+        null_ls.builtins.formatting.asmfmt,
+        ----------------                              ----------------
+
+        -------------------        PYTHON       ------------------------
+        -- null_ls.builtins.formatting.black,
+        -- null_ls.builtins.formatting.isort,
+        ----------------                              ----------------
+
+        -------------------        XML       ------------------------
+        null_ls.builtins.formatting.tidy,
+        null_ls.builtins.diagnostics.tidy,
+        ----------------                              ----------------
+
+        -------------------        MARKDOWN       ------------------------
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.diagnostics.markdownlint,
+        ----------------                              ----------------
+
+        -------------------        TOML       ------------------------
+        -- formatting -- ***WE USE TAPLO***
+        --  diagnostics -- ***WE USE TAPLO***
+        ----------------                              ----------------
+      },
+    })
+
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+  end,
 }
-
--- diagnostic == linting
--- formatting == format
 
 -- diagnostic == linting
 -- formatting == format
