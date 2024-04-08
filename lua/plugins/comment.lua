2cf479e3b8818c27c07b0998c678933b1eed1c50
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-08 14:11:56 -0700

    added react and nextjs features

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,42 +1,41 @@
 return {
   "numToStr/Comment.nvim",
+  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
   config = function()
-    require('Comment').setup {
-      padding = true,  ---Add a space b/w comment and the line
-      sticky = true,   ---Whether the cursor should stay at its position
-      ignore = nil,    ---Lines to be ignored while (un)comment
+    -- require("ts_context_commentstring").setup()
+    require("ts_context_commentstring").setup({
+      enable_autocmd = true,
+    })
+    require("Comment").setup({
+      padding = true,
+      sticky = true,
+      ignore = nil,
 
-      toggler = {      ---LHS of toggle mappings in NORMAL mode
-        line = 'gcc',  ---Line-comment toggle keymap
-        block = 'gbc', ---Block-comment toggle keymap
+      toggler = {
+        line = "gcc",
+        block = "gbc",
       },
 
-      opleader = {    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-        line = 'gc',  ---Line-comment keymap
-        block = 'gb', ---Block-comment keymap
+      opleader = {
+        line = "gc",
+        block = "gb",
       },
 
-      extra = {        ---LHS of extra mappings
-        above = 'gcO', ---Add comment on the line above
-        below = 'gco', ---Add comment on the line below
-        eol = 'gcA',   ---Add comment at the end of line
+      extra = {
+        above = "gcO",
+        below = "gco",
+        eol = "gcA",
       },
       mappings = {
         basic = true,
         extra = true,
       },
-      pre_hook = nil,
-      post_hook = nil,
-    }
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    })
 
-
-   -- Instead of `gcc` we do <space>/
+    -- Instead of `gcc` we do <space>/
     -- Normal mode mapping for <Space>/
-    vim.api.nvim_set_keymap('n', '<Leader>/', '<Plug>(comment_toggle_linewise_current)', { silent = true })
-
-    -- Visual mode mapping for <Space>/
-    vim.api.nvim_set_keymap('x', '<Leader>/', '<Plug>(comment_toggle_linewise_visual)', { silent = true })
-
-  end
+    vim.api.nvim_set_keymap("n", "<Leader>/", "<Plug>(comment_toggle_linewise_current)", { silent = true })
+    vim.api.nvim_set_keymap("x", "<Leader>/", "<Plug>(comment_toggle_linewise_visual)", { silent = true })
+  end,
 }
-
