c67db1a67707bc6c7ce1a6876c3c504e0be95a0a
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-08 14:11:07 -0700

    prettier format for all possible langugaes js,ts,css,html,yaml,md,toml and more

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,68 +1,101 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "gbprod/none-ls-shellcheck.nvim", -- shellcheck
-    "nvimtools/none-ls-extras.nvim", -- eslint_d
-  },
-  config = function()
-    -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -------------------        JAVA       ------------------------
-        -- formatting -- ***WE USE FTPLUGIN/JAVA.LUA***
-        --  diagnostics -- ***WE USE FTPLUGIN/JAVA.LUA***
-        ----------------                              ----------------
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"gbprod/none-ls-shellcheck.nvim", -- shellcheck
+		"nvimtools/none-ls-extras.nvim", -- eslint_d
+	},
+	config = function()
+		-- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-------------------        JAVA       ------------------------
+				-- formatting -- ***WE USE FTPLUGIN/JAVA.LUA***
+				--  diagnostics -- ***WE USE FTPLUGIN/JAVA.LUA***
+				----------------                              ----------------
 
-        -------------------        LUA       ------------------------
-        null_ls.builtins.formatting.stylua,
-        --  diagnostics -- ***WE USE lua_ls**
-        ----------------                              ----------------
+				-------------------        LUA       ------------------------
+				null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.diagnostics.selene,
+				--  diagnostics -- ***WE USE lua_ls**
+				----------------                              ----------------
 
-        -------------------        BASH       ------------------------
-        null_ls.builtins.formatting.shfmt,
-        require("none-ls-shellcheck.diagnostics"),
-        require("none-ls-shellcheck.code_actions"),
-        ----------------                              ----------------
+				-------------------        BASH       ------------------------
+				null_ls.builtins.formatting.shfmt,
+				require("none-ls-shellcheck.diagnostics"),
+				require("none-ls-shellcheck.code_actions"),
+				----------------                              ----------------
 
-        -------------------        TS | JS       ------------------------
-        null_ls.builtins.formatting.prettierd,
-        require("none-ls.diagnostics.eslint_d"), -- TS | JS
-        -------------------        HTML       ------------------------
-        null_ls.builtins.diagnostics.markdownlint, -- HTML
-        -------------------        CSS       ------------------------
-        null_ls.builtins.diagnostics.stylelint, -- CSS
-        ----------------                              ----------------
+				-------------------        TS | JS       ------------------------
+				-- formatting: PRETTIER FORMATTER
+				require("none-ls.diagnostics.eslint_d"), -- TS | JS
+				-------------------        HTML       ------------------------
+				null_ls.builtins.diagnostics.markdownlint, -- HTML
+				-------------------        CSS       ------------------------
+				null_ls.builtins.diagnostics.stylelint, -- CSS
+				----------------                              ----------------
 
-        -------------------        GO       ------------------------
-        null_ls.builtins.diagnostics.staticcheck,
-        null_ls.builtins.formatting.asmfmt,
-        ----------------                              ----------------
+				-------------------        GO       ------------------------
+				null_ls.builtins.diagnostics.staticcheck,
+				null_ls.builtins.formatting.asmfmt,
+				----------------                              ----------------
 
-        -------------------        PYTHON       ------------------------
-        -- null_ls.builtins.formatting.black,
-        -- null_ls.builtins.formatting.isort,
-        ----------------                              ----------------
+				-------------------        FISH       ------------------------
+				null_ls.builtins.diagnostics.fish,
+				----------------                              ----------------
 
-        -------------------        XML       ------------------------
-        null_ls.builtins.formatting.tidy,
-        null_ls.builtins.diagnostics.tidy,
-        ----------------                              ----------------
+				-------------------        PYTHON       ------------------------
+				-- null_ls.builtins.formatting.black,
+				-- null_ls.builtins.formatting.isort,
+				----------------                              ----------------
 
-        -------------------        MARKDOWN       ------------------------
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.markdownlint,
-        ----------------                              ----------------
+				-----------------        XML       ------------------------
+				null_ls.builtins.formatting.tidy.with({
+					filetypes = { "xml" },
+				}),
+				null_ls.builtins.diagnostics.tidy.with({
+					filetypes = { "xml" },
+				}),
+				----------------                              ----------------
 
-        -------------------        TOML       ------------------------
-        -- formatting -- ***WE USE TAPLO***
-        --  diagnostics -- ***WE USE TAPLO***
-        ----------------                              ----------------
-      },
-    })
+				-------------------        MARKDOWN       ------------------------
+				-- formatting: PRETTIER FORMATTER
+				-- null_ls.builtins.diagnostics.markdownlint, -- BREAKS MARKDOWN
+				----------------                              ----------------
 
-    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-  end,
+				-------------------        TOML       ------------------------
+				-- formatting: PRETTIER FORMATTER
+				--  diagnostics -- ***WE USE TAPLO***
+				----------------                              ----------------
+
+				-------------------        PRETTIER FORMATTER       ------------------------
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = {
+						"javascript",
+						"javascriptreact",
+						"typescript",
+						"typescriptreact",
+						"vue",
+						"css",
+						"scss",
+						"less",
+						"html",
+						"json",
+						"jsonc",
+						"yaml",
+						"markdown",
+						"markdown.mdx",
+						"graphql",
+						"handlebars",
+					},
+					extra_filetypes = { "toml", "svelte" },
+				}),
+				----------------                              ----------------
+			},
+		})
+
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+	end,
 }
 
 -- diagnostic == linting
