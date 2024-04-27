90ca56582ce446a564c765534d6020a5aaf6c45c
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-26 20:24:27 -0700

    huge update to auto complete, new addions, new scripts, new plugs

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -3,22 +3,24 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",              -- source for LSP completions ** IMPORTANT **
-      "hrsh7th/cmp-buffer",                -- source for text in buffer
-      "hrsh7th/cmp-path",                  -- source for file system paths in commands
-      "L3MON4D3/LuaSnip",                  -- snippet engine
-      "saadparwaiz1/cmp_luasnip",          -- for lua autocompletion
-      "rafamadriz/friendly-snippets",      -- useful snippets library
-      "onsails/lspkind.nvim",              -- vs-code like pictograms
+      "hrsh7th/cmp-nvim-lsp",                 -- source for LSP completions ** IMPORTANT **
+      "hrsh7th/cmp-buffer",                   -- source for text in buffer
+      "hrsh7th/cmp-path",                     -- source for file system paths in commands
+      "L3MON4D3/LuaSnip",                     -- snippet engine
+      "saadparwaiz1/cmp_luasnip",             -- for lua autocompletion
+      "rafamadriz/friendly-snippets",         -- useful snippets library
+      "onsails/lspkind.nvim",                 -- vs-code like pictograms
       "hrsh7th/cmp-nvim-lsp-document-symbol", -- document symbols
-      "hrsh7th/cmp-nvim-lsp-signature-help", -- signature help
+      "hrsh7th/cmp-nvim-lsp-signature-help",  -- signature help
+
       {
-        "VonHeikemen/lsp-zero.nvim",       -- TAB | SHIFT-TAB to navigate through snippets
+        "VonHeikemen/lsp-zero.nvim", -- TAB | SHIFT-TAB to navigate through snippets
         branch = "v2.x",
       },
     },
     config = function()
       local lspkind = require("lspkind") -- VSCODE
+
       local cmp_action = require("lsp-zero").cmp_action()
       local cmp = require("cmp")
 
@@ -51,14 +53,14 @@ return {
           { name = "luasnip" }, -- For Snippets
           { name = "nvim_lsp_document_symbol" },
           { name = "nvim_lsp_signature_help" },
-          -- { name = "buffer" }, -- text within current buffer -- DISLIKE THIS VERY MUCH 
+          -- { name = "buffer" }, -- text within current buffer -- DISLIKE THIS VERY MUCH
           { name = "path" }, -- file system paths
         }),
         formatting = {
           format = lspkind.cmp_format({
             -- mode = "symbol",    -- show only symbol annotations
-            maxwidth = 50,      -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            maxwidth = 50,            -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = "...",    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
             show_labelDetails = true, -- show labelDetails in menu. Disabled by default
             symbol_map = {
               Copilot = "",
