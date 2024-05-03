6b79e2072eb78f35ae2b743fe8606963834d3d69
Angel J. Haro <haro.j.angel@gmail.com>
2024-05-02 23:31:07 -0700

    Linter|Formatter: PHP

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,114 +1,121 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"gbprod/none-ls-shellcheck.nvim", -- shellcheck
-		"nvimtools/none-ls-extras.nvim", -- eslint_d
-	},
-	config = function()
-		-- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-------------------        JAVA       ------------------------
-				-- formatting -- ***WE USE FTPLUGIN/JAVA.LUA***
-				--  diagnostics -- ***WE USE FTPLUGIN/JAVA.LUA***
-				----------------                              ----------------
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "gbprod/none-ls-shellcheck.nvim", -- shellcheck
+    "nvimtools/none-ls-extras.nvim", -- eslint_d
+  },
+  config = function()
+    -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        -------------------        JAVA       ------------------------
+        -- formatting -- ***WE USE FTPLUGIN/JAVA.LUA***
+        --  diagnostics -- ***WE USE FTPLUGIN/JAVA.LUA***
+        ----------------                              ----------------
 
-				-------------------        TESTING       ------------------------
-				-- formatting -- ***WE USE FTPLUGIN/JAVA.LUA***
-				-- null_ls.builtins.formatting.google_java_format,
-				-- null_ls.builtins.diagnostics.checkstyle.with({
-				--       extra_args = { "-c", "/Users/aharo/.local/share/nvim/java-stuff/intellij-java-google-style.xml" },
-				--     }),
-				----------------                              ----------------
+        -------------------        LUA       ------------------------
+        null_ls.builtins.formatting.stylua,
+        -- null_ls.builtins.diagnostics.selene,
+        --  diagnostics -- ***WE USE lua_ls**
+        ----------------                              ----------------
 
-				-------------------        LUA       ------------------------
-				null_ls.builtins.formatting.stylua,
-				-- null_ls.builtins.diagnostics.selene,
-				--  diagnostics -- ***WE USE lua_ls**
-				----------------                              ----------------
+        -------------------        BASH       ------------------------
+        null_ls.builtins.formatting.shfmt,
+        require("none-ls-shellcheck.diagnostics"),
+        require("none-ls-shellcheck.code_actions"),
+        ----------------                              ----------------
 
-				-------------------        BASH       ------------------------
-				null_ls.builtins.formatting.shfmt,
-				require("none-ls-shellcheck.diagnostics"),
-				require("none-ls-shellcheck.code_actions"),
-				----------------                              ----------------
+        -------------------        TS | JS       ------------------------
+        -- formatting: PRETTIER FORMATTER
+        require("none-ls.diagnostics.eslint_d"), -- TS | JS
+        require("none-ls.code_actions.eslint_d"),
+        require("none-ls.formatting.beautysh"),
+        require("none-ls.formatting.eslint_d"),
+        require("none-ls.formatting.jq"),
+        ----------------                              ----------------
 
-				-------------------        TS | JS       ------------------------
-				-- formatting: PRETTIER FORMATTER
-				require("none-ls.diagnostics.eslint_d"), -- TS | JS
-				-------------------        HTML       ------------------------
-				null_ls.builtins.diagnostics.markuplint,
-				-------------------        CSS       ------------------------
-				null_ls.builtins.diagnostics.stylelint, -- CSS
-				----------------                              ----------------
+        -------------------        HTML       ------------------------
+        -- null_ls.builtins.diagnostics.markuplint, -- TOO MANY ISSUES!!!!!!!!!!!
+        ----------------                              ----------------
 
-				-------------------        GO       ------------------------
-				null_ls.builtins.diagnostics.staticcheck,
-				null_ls.builtins.formatting.asmfmt,
-				----------------                              ----------------
+        -------------------        CSS       ------------------------
+        null_ls.builtins.diagnostics.stylelint, -- CSS
+        ----------------                              ----------------
 
-				-------------------        FISH       ------------------------
-				null_ls.builtins.diagnostics.fish,
-				----------------                              ----------------
+        -------------------        GO       ------------------------
+        null_ls.builtins.diagnostics.staticcheck,
+        null_ls.builtins.formatting.asmfmt,
+        ----------------                              ----------------
 
-				-------------------        PYTHON       ------------------------
-				-- null_ls.builtins.formatting.black,
-				-- null_ls.builtins.formatting.isort,
-				----------------                              ----------------
+        -------------------        PHP       ------------------------
+        null_ls.builtins.diagnostics.phpstan,
+        null_ls.builtins.formatting.phpcsfixer,
+        ----------------                              ----------------
 
-				-----------------        XML       ------------------------
-				null_ls.builtins.formatting.tidy.with({
-					filetypes = { "xml" },
-				}),
-				null_ls.builtins.diagnostics.tidy.with({
-					filetypes = { "xml" },
-				}),
-				----------------                              ----------------
+        -------------------        FISH       ------------------------
+        null_ls.builtins.diagnostics.fish,
+        ----------------                              ----------------
 
-				-------------------        SQL       ------------------------
-				-- null_ls.builtins.formatting.sql_formatter, -- WORKS BUT NO LSP ELSE BREAKS
-				null_ls.builtins.formatting.pg_format, -- WORKS BUT NO LSP ELSE BREAKS
-				----------------                              ----------------
+        -------------------        PYTHON       ------------------------
+        null_ls.builtins.formatting.black,
+        -- null_ls.builtins.formatting.isort,
+        null_ls.builtins.diagnostics.flake8,
+        ----------------                              ----------------
 
-				-------------------        MARKDOWN       ------------------------
-				-- formatting: PRETTIER FORMATTER
-				-- null_ls.builtins.diagnostics.markdownlint, -- BREAKS MARKDOWN
-				----------------                              ----------------
+        -----------------        XML       ------------------------
+        null_ls.builtins.formatting.tidy.with({
+          filetypes = { "xml" },
+        }),
+        null_ls.builtins.diagnostics.tidy.with({
+          filetypes = { "xml" },
+        }),
+        ----------------                              ----------------
+        ----------------                              ----------------
 
-				-------------------        TOML       ------------------------
-				-- formatting: PRETTIER FORMATTER
-				--  diagnostics -- ***WE USE TAPLO***
-				----------------                              ----------------
+        -------------------        SQL       ------------------------
+        -- null_ls.builtins.formatting.sql_formatter, -- WORKS BUT NO LSP ELSE BREAKS
+        null_ls.builtins.formatting.pg_format, -- WORKS BUT NO LSP ELSE BREAKS
+        ----------------                              ----------------
 
-				-------------------        PRETTIER FORMATTER       ------------------------
-				null_ls.builtins.formatting.prettierd.with({
-					filetypes = {
-						"javascript",
-						"javascriptreact",
-						"typescript",
-						"typescriptreact",
-						"vue",
-						"css",
-						"scss",
-						"less",
-						"html",
-						"json",
-						"jsonc",
-						"yaml",
-						"markdown",
-						"markdown.mdx",
-						"graphql",
-						"handlebars",
-					},
-					extra_filetypes = { "toml", "svelte" },
-				}),
-				----------------                              ----------------
-			},
-		})
+        -------------------        MARKDOWN       ------------------------
+        -- formatting: PRETTIER FORMATTER
+        -- null_ls.builtins.diagnostics.markdownlint, -- BREAKS MARKDOWN
+        ----------------                              ----------------
 
-		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-	end,
+        -------------------        TOML       ------------------------
+        -- formatting: PRETTIER FORMATTER
+        --  diagnostics -- ***WE USE TAPLO***
+        ----------------                              ----------------
+
+        -------------------        PRETTIER FORMATTER       ------------------------
+        null_ls.builtins.formatting.prettierd.with({
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "vue",
+            "css",
+            "scss",
+            "less",
+            "html",
+            "json",
+            "jsonc",
+            "yaml",
+            "markdown",
+            "markdown.mdx",
+            "graphql",
+            "handlebars",
+          },
+          extra_filetypes = { "toml", "svelte" },
+        }),
+        ----------------                              ----------------
+      },
+    })
+
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+  end,
 }
 
 -- diagnostic == linting
